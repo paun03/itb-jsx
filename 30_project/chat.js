d9b78328c9a38faf8d77d428c8217db59252f0ce
paun03 <paunovicm2003@gmail.com>
2024-02-12 17:24:58 +0100

    commit

--- 30_project/chat.js
+++ 30_project/chat.js
@@ -69,7 +69,7 @@ class Chatroom {
                         const documentId = change.doc.id; 
                         documentData.id = documentId;
                         callback(documentData);
-                    }
+                    }   
                 });
             });
     }
